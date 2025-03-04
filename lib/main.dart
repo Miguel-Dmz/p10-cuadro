b94b59edf04fdbe0b316978b0dd2372164bcc295
MIGUEL_ANGEL_DOMINGUEZ_LOPEZ <a.22308051281173@cbtis128.edu.mx>
2025-03-04 16:52:11 +0000

    Dominguez cuadrado

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,46 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Color(0xff61d2fb),
+          title: Text('Miguel Dominguez \n Mat:22308051281173'),
+        ),
+        body: Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20), // Bordes redondeados
+            child: Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: Color(0xff03d603), // Verde fosforescente
+                    width: 8,
+                  ),
+                  bottom: BorderSide(
+                    color: Color(0xff01dddd), // Azul fosforescente
+                    width: 8,
+                  ),
+                  left: BorderSide(
+                    color: Color(0xff03d603), // Verde fosforescente
+                    width: 8,
+                  ),
+                  right: BorderSide(
+                    color: Color(0xff01dddd), // Azul fosforescente
+                    width: 8,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
